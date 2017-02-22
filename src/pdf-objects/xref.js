417991aa536b27268a291abbbf4162308027c31c
Jason Galea <jason@lecstor.com>
2017-02-23 07:39:54 +1000

    fix xref

--- src/pdf-objects/xref.js
+++ src/pdf-objects/xref.js
@@ -5,15 +5,15 @@ import _map from 'lodash/map';
 
 const xref = {
   toPDF () {
-    const lines = [
-      'xref',
-      `0 ${this.offsets.length + 1}`,
-      '0000000000 65535 f '
-    ];
     // sort offsets by object ids
     const offsets = _map(
       _keys(this.offsets).sort((a, b) => +a - +b), id => this.offsets[id]
     );
+    const lines = [
+      'xref',
+      `0 ${offsets.length + 1}`,
+      '0000000000 65535 f '
+    ];
     offsets.forEach(offset => {
       const formatted = `0000000000${offset}`.slice(-10);
       lines.push(`${formatted} 00000 n `);
